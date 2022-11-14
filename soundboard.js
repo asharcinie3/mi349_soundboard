ce3b2ac11253fa4d0d43218390e599228a18ba5e
asharcinie3 <ashley.arciniegga@gmail.com>
2022-11-14 18:27:18 -0500

    add onmouse over

--- soundboard.js
+++ soundboard.js
@@ -5,6 +5,13 @@ document.getElementById("b1").onclick = () => {
     audio.play();
   };
 
+  document.getElementById("b1").onmouseover = () => {
+    const audio = new Audio(
+      "bass.mp3"
+    );
+    audio.play();
+  };
+
   document.getElementById("b2").onclick = () => {
     const audio = new Audio(
       "snare.mp3"
@@ -12,7 +19,21 @@ document.getElementById("b1").onclick = () => {
     audio.play();
   };
 
+  document.getElementById("b2").onmouseover = () => {
+    const audio = new Audio(
+      "snare.mp3"
+    );
+    audio.play();
+  };
+
   document.getElementById("b3").onclick = () => {
+    const audio = new Audio(
+      "cymbal.mp3"
+    );
+    audio.play();
+  };
+  
+  document.getElementById("b3").onmouseover = () => {
     const audio = new Audio(
       "cymbal.mp3"
     );
