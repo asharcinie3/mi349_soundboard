cc90faa032f42289a7ef36bd36c4c61e026d03ec
asharcinie3 <ashley.arciniegga@gmail.com>
2022-11-14 19:01:16 -0500

    add sounds

--- soundboard.js
+++ soundboard.js
@@ -39,3 +39,45 @@ document.getElementById("b1").onclick = () => {
     );
     audio.play();
   };
+
+  document.getElementById("b4").onclick = () => {
+    const audio = new Audio(
+      "guitar.mp3"
+    );
+    audio.play();
+  };
+
+  document.getElementById("b4").onmouseover = () => {
+    const audio = new Audio(
+      "guitar.mp3"
+    );
+    audio.play();
+  };
+
+  document.getElementById("b5").onclick = () => {
+    const audio = new Audio(
+      "accordian.mp3"
+    );
+    audio.play();
+  };
+
+  document.getElementById("b5").onmouseover = () => {
+    const audio = new Audio(
+      "accordian.mp3"
+    );
+    audio.play();
+  };
+
+  document.getElementById("b6").onclick = () => {
+    const audio = new Audio(
+      "flute.wav"
+    );
+    audio.play();
+  };
+
+  document.getElementById("b6").onmouseover = () => {
+    const audio = new Audio(
+      "flute.wav"
+    );
+    audio.play();
+  };
